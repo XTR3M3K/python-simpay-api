dd455e499f0b7ff75725e836400932f55893b41f
Mateusz Kucz <mati.kucz65@gmail.com>
2020-08-10 03:17:52 +0200

    Initial commit Added direct_billing.py Added sms.py Added necessary files for distribution

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='simpay-api',
+    version='1.0',
+    description='Python wrapper for Simpay API',
+    author='XTR3M3K <mati.kucz65@gmail.com, Lenistwo',
+
+    url='https://github.org/XTR3M3K/python-simpay-api',
+
+    packages=find_packages(),
+    install_required=[
+        'requests'
+    ]
+)
